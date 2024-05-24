9890f65473148b98ae8c572bdfc6fadbeba0b337
Opilane TTHK <opilane@tthk.ee>
2024-05-24 15:46:27 +0300

    In progress

---
+++ Maui/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Pages;
+
+namespace Maui
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnNavigateButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+
+}
